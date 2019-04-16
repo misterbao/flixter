729dcf39a3303536bd7d5fb9dd37be02c5b99858
misterbao <mister.bao@gmail.com>
2019-04-16 00:57:38 +0000

    flixter 15 listing courses correction

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-  has_many :courses
-
+    has_many :courses
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
 end
