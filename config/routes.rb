e56d4d6dd234251e1b324f7f21fdd82cbe26b857
misterbao <mister.bao@gmail.com>
2019-04-23 05:52:16 +0000

    flixter 39 adding static page

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   resource :dashboard, only: [:show]
   root 'static_pages#index'
   get 'privacy', to: 'static_pages#privacy'
-  get 'team', to: 'static_pages#team'
-  get 'careers', to: 'static_pages#careers'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
@@ -12,11 +10,10 @@ Rails.application.routes.draw do
   namespace :instructor do
     resources :lessons, only: [:update]
     resources :sections, only: [:update] do
-      resources :lessons, only: [:create] 
+      resources :lessons, only: [:create]
     end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create]
     end
-  end 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
